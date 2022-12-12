30dd0acc0119dbb22c7578bcf7a1bf9031918987
hemzi <aaron.stacey@proton.me>
2022-12-12 14:32:17 -0600

    checkpoint

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const TOKEN = document.querySelector("input#token").value;
-
 const app = Vue.createApp({
   data() {
     return {
@@ -8,7 +6,7 @@ const app = Vue.createApp({
     };
   },
   methods: {
-    async getDevices() {
+    async getDevices(filterType) {
       const res = await fetch("https://webapi.teamviewer.com/api/v1/devices", {
         method: "GET",
         headers: {
@@ -18,14 +16,37 @@ const app = Vue.createApp({
       });
       const json = await res.json();
 
-      // filter for devices that have been offline for extended time.
-      const filtered = await json.devices.filter(
-        (device) =>
-          device.online_state === "Offline" && device.last_seen === undefined
-      );
+      // format last_seen date
+      json.devices.map((device) => {
+        if (device.last_seen !== undefined) {
+          device.last_seen = new Date(
+            Date.parse(device.last_seen)
+          ).toLocaleDateString("en-US");
+        }
+      });
 
-      this.devices = filtered;
-      this.deviceCount = filtered.length;
+      if (arguments.length === undefined) {
+        return (this.devices = json.devices);
+      }
+
+      // all offline devices
+      if (filterType == "offline") {
+        const filtered = await json.devices.filter(
+          (device) => device.online_state === "Offline"
+        );
+
+        // return (this.devices = filtered);
+        return (this.devices = filtered), (this.deviceCount = filtered.length);
+      }
+      // filter for devices that have been offline for extended time.
+      if (filterType == "extended") {
+        const filtered = await json.devices.filter(
+          (device) =>
+            device.online_state === "Offline" && device.last_seen === undefined
+        );
+
+        return (this.devices = filtered);
+      }
     },
     async removeDevice(id) {
       const res = await fetch(
@@ -43,10 +64,39 @@ const app = Vue.createApp({
         console.log(`REMOVEd DEVICE WITH ID: ${id}`);
       }
     },
+    async fixName(id, alias) {
+      let newName = alias;
+      console.log(alias);
+      newName = newName.replace("FCC-", "");
+      newName = newName.replace(/([A-Z,a-z][0-9]*)+\-/, "");
+      if (newName.match(/^\d{1,6} (\w*) (\w*)$/)) {
+        newName = newName.replace(/^(\d{1,6}) (\w*) (\w*)$/, "$1 ($2 $3)");
+      }
+
+      console.log(newName);
+      const index = this.devices.findIndex((device) => {
+        return device.device_id === id;
+      });
+
+      console.log(index);
+      if (index !== -1) {
+        this.devices[index].alias = newName;
+      }
+      // const res = await fetch(
+      //   `https://webapi.teamviewer.com/api/v1/devices/${id}`,
+      //   {
+      //     method: "PUT",
+      //     headers: {
+      //       Authorization: `Bearer ${TOKEN}`,
+      //       "Content-Type": "application/json",
+      //     },
+      //   }
+      // );
+    },
   },
-  // mounted() {
-  //   this.getDevices();
-  // },
+  mounted() {
+    this.getDevices("offline");
+  },
 });
 
 app.mount("#app");
